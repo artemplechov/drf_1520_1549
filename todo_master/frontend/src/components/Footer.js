b8d8d29674bdf39cf9ce7be004d008df9f745140
artemplechov <artemplechov@gmail.com>
2022-03-27 19:59:15 +0300

    lesson 2

---
+++ todo_master/frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer class="footer">
+            <p>My Footer
+            <a href="mailto:admin@yandex.ru">admin@yandex.ru</a></p>
+        </footer>
+    )
+}
+export default Footer
---
+++ todo_master/frontend/src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Menu = () => {
+    return (
+        <ul class="menu-main">
+            <li><a href="" class="current">Work</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    )
+}
+export default Menu
