3ab4c9d2f261c003f01e7d9c6c4e089b2a53681e
artemplechov <artemplechov@gmail.com>
2022-04-05 02:11:07 +0300

    lesson 3

--- todo_master/todo_master/urls.py
+++ todo_master/todo_master/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from users.views import UserModelViewSet
+from users.views import UserModelViewSet, ProjectModelViewSet, TODOModelViewSet
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
+router.register('projects', ProjectModelViewSet)
+router.register('TODO', TODOModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
