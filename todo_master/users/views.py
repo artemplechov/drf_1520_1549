af75262c503129fb2239ba0c540f192b21ee74d8
artemplechov <artemplechov@gmail.com>
2022-04-12 01:47:06 +0300

    lesson 4

--- todo_master/users/views.py
+++ todo_master/users/views.py
@@ -1,13 +1,19 @@
-from rest_framework.renderers import JSONRenderer, AdminRenderer
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import UpdateAPIView, RetrieveAPIView
+from rest_framework.mixins import ListModelMixin
+from rest_framework.renderers import JSONRenderer, AdminRenderer, BrowsableAPIRenderer
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 from .models import User
 from .serializers import UserModelSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+#class UserModelViewSet(ModelViewSet):
+#    queryset = User.objects.all()
+#    serializer_class = UserModelSerializer
+
+class UserCustomViewSet(ListModelMixin,RetrieveAPIView,UpdateAPIView,GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
-
+    renderer_class = [JSONRenderer,BrowsableAPIRenderer]
 
 
